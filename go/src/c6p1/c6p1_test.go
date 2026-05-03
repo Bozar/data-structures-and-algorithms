0c7ea14dda232d4754349cae89f7761b920d4a9d
bozar <5583771+Bozar@users.noreply.github.com>
2026-05-03 21:43:53 +0800

    6.1: Solved.

---
+++ go/src/c6p1/c6p1_test.go
@@ -0,0 +1,25 @@
+package c6p1_test
+
+import (
+	"testing"
+
+	"github.com/Bozar/data-structures-and-algorithms-made-easy/go/src/c6p1"
+	"github.com/Bozar/data-structures-and-algorithms-made-easy/go/src/tree"
+)
+
+func TestMaxValue(t *testing.T) {
+	var root *tree.Node
+	var nodes []*tree.Node
+	var node *tree.Node
+	var maxVal int
+	for i := 0; i < 10; i++ {
+		root = &tree.Node{}
+		nodes = root.RandBtree(int64(42+i), 5, 99)
+		node = nodes[int(float64(len(nodes)*(i+1))*0.1)-1]
+		node.Data = 100
+		maxVal = c6p1.MaxValue(*root)
+		if maxVal != 100 {
+			t.Errorf("Wrong max: %d", maxVal)
+		}
+	}
+}
