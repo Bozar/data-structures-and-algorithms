b83dd882cb9256c87dd5588dd01c0354ffdfb5c7
bozar <5583771+Bozar@users.noreply.github.com>
2026-05-21 16:29:33 +0800

    6.30: Solved.

---
+++ go/src/c6p30/c6p30_test.go
@@ -0,0 +1,39 @@
+package c6p30_test
+
+import (
+	"testing"
+
+	"github.com/Bozar/data-structures-and-algorithms-made-easy/go/src/c6p30"
+	"github.com/Bozar/data-structures-and-algorithms-made-easy/go/src/tree"
+)
+
+func TestZigzagTraverse(t *testing.T) {
+	nodes := []tree.Node{}
+	for i := 1; i < 8; i++ {
+		nodes = append(nodes, tree.Node{})
+		nodes[len(nodes)-1].Data = i
+	}
+	root := nodes[0]
+	root.Left = &nodes[1]
+	root.Right = &nodes[2]
+	(*root.Left).Left = &nodes[3]
+	(*root.Left).Right = &nodes[4]
+	(*root.Right).Left = &nodes[5]
+	(*root.Right).Right = &nodes[6]
+	//	root.Print()
+
+	check := []int{
+		1, 3, 2, 4, 5, 6, 7,
+	}
+	output := c6p30.ZigzagTraverse(root)
+	if len(output) != len(check) {
+		t.Errorf("Len: %d-%d", len(check), len(output))
+		return
+	}
+	for k, v := range check {
+		if v != output[k] {
+			t.Errorf("Idx: %d, Val: %d", k, output[k])
+			return
+		}
+	}
+}
