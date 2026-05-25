9ca645bf2cc2bf18af785e0dd095201b2ab27d25
bozar <5583771+Bozar@users.noreply.github.com>
2026-05-25 14:52:06 +0800

    6.47: Solved.

---
+++ go/src/c6p47/c6p47.go
@@ -0,0 +1,16 @@
+package c6p47
+
+import (
+	"github.com/Bozar/data-structures-and-algorithms-made-easy/go/src/tree"
+)
+
+func LCA(root *tree.Node, this int, that int) int {
+	if ((this <= root.Data) && (that >= root.Data)) ||
+		((this >= root.Data) && (that <= root.Data)) {
+		return root.Data
+	} else if this <= root.Data {
+		return LCA(root.Left, this, that)
+	} else {
+		return LCA(root.Right, this, that)
+	}
+}
